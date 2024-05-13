49aca9fa7646d2367c2baf4e841933371b729bd0
Prachi Gupta <priyaprachi905@gmail.com>
2024-05-13 12:25:46 +0530

    some-changes

--- QuickmartClient/src/request-methods.js
+++ QuickmartClient/src/request-methods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000/api';
+const BASE_URL = 'https://quickmartecommercewebsite.onrender.com/api';
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGEzOGUwYjczZTg5NTczZDAxNmZmNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MzA3NTI3OSwiZXhwIjoxNjYzMTYxNjc5fQ.sLeBTfgAymobkEF1rcOzexB8-ixEXKWat-7lRFfaKVQ';
 
 export const publicRequest = axios.create(
